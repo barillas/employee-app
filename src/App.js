68dd8787335ed635bfe5717fe836d9c0eb4c747e
Oscar Barillas <oscarbarillas93@gmail.com>
2016-11-20 18:01:42 -0800

    Login Form started

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
+  componentWillMount() {
+    var config = {
+    apiKey: "AIzaSyCkxPRCboVlk-FQcWAHAlNhYElOXlEXgUA",
+    authDomain: "manager-45457.firebaseapp.com",
+    databaseURL: "https://manager-45457.firebaseio.com",
+    storageBucket: "manager-45457.appspot.com",
+    messagingSenderId: "872799032531"
+  };
+  firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>
-            Hello!
-          </Text>
+          <LoginForm />
         </View>
       </Provider>
     );
